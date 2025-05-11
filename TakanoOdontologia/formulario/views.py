1c38fbb90dd9f77cb53e3fa826b723c652c438fc
Kelvin Marques Cordeiro <kelvin_marques@outlook.com>
2025-05-11 19:04:11 -0300

    Modify from email for the last test

--- TakanoOdontologia/formulario/views.py
+++ TakanoOdontologia/formulario/views.py
@@ -78,8 +78,8 @@ def enviar_email(contato):
             Tipo de consulta: {contato.tipo_consulta}
             Horário preferido: {contato.horario_preferencia}
             """,
-            'no-reply@example.com',
-            ['secretaria@takanoOdontologia.com'],  # Ajuste com o e-mail real do cliente
+            'no-reply@takanoOdondologia.com',
+            ['kellgamerm@gmail.com'],  # Ajuste com o e-mail real do cliente
             fail_silently=True,
             html_message=mensagem_html 
             )
