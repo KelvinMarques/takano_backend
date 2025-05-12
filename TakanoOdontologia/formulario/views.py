b0e18213c374cd6c2c91baee3f0d83c5cccaeb15
Kelvin Marques Cordeiro <kelvin_marques@outlook.com>
2025-05-12 13:21:08 -0300

    testing send email forms with email personal

--- TakanoOdontologia/formulario/views.py
+++ TakanoOdontologia/formulario/views.py
@@ -78,7 +78,7 @@ def enviar_email(contato):
             Tipo de consulta: {contato.tipo_consulta}
             Horário preferido: {contato.horario_preferencia}
             """,
-            'kelvin_marques@outlook.com',
+            'takanoodontologia@hotmail.com',
             ['kapy.software@gmail.com'],  # Ajuste com o e-mail real do cliente
             fail_silently=True,
             html_message=mensagem_html 
